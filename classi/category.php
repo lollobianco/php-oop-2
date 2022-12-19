b04d8f57f4c79e607790f4a61fb72ffd3e5dbc15
lollobianco <loreb2002@gmail.com>
2022-12-19 13:38:14 +0100

    File compilati

---
+++ classi/category.php
@@ -0,0 +1,18 @@
+<?php 
+
+class Categoria{
+   public $nome;
+   public $icon;
+
+   public function __construct(
+      String $nome,
+      String $icon,)
+   {
+      
+      $this -> nome = $nome;
+      $this -> icon = $icon;
+
+   }
+}
+
+?>
---
+++ classi/cibo.php
@@ -0,0 +1,25 @@
+<?php 
+
+include_once __DIR__ . "./prodotto.php";
+class Cibo extends Prodotto{
+
+   public $peso;
+   public $ingredienti;
+
+   public function __construct(
+      String $immagine,
+      String $nome,
+      Float $prezzo,
+      Categoria $category,
+      
+      Float $peso,
+      String $ingredienti,)
+   {
+      $this -> peso = $peso;
+      $this->ingredienti = $ingredienti;
+      parent::__construct($immagine, $nome, $prezzo, $category);
+   }
+
+}
+
+?>
